239752f7513aebc524ddca7254e016e03b49abf8
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-10 22:31:52 -0300

    manipulando_o_DOM_adicionando_icone_de_remocao_aos_elementos

--- removeChild/js/script.js
+++ removeChild/js/script.js
@@ -4,11 +4,17 @@ const array_skill = ["Javascript", "PHP", "Laravel", "ReactJs", "VueJs", "HTML",
 array_skill.map((el, chave)=> {
     const novo_elemento = document.createElement("div");    
     novo_elemento.setAttribute("id", "skill" + chave);
-    novo_elemento.setAttribute("class", "hardskill");    
+    novo_elemento.setAttribute("class", "hardskill");  
+    
+    const btn_lixeira = document.createElement("img");      // criando um novo elemento (imagem)
+    btn_lixeira.setAttribute("src", "./lixeira.png");           // definindo o atributo 'src' que aponta para a imagem da lixeira
+    btn_lixeira.setAttribute("class", "./btn_lixeira");
+    novo_elemento.appendChild(btn_lixeira);     // anexando a imagem ao 'novo_elemento'
+
     novo_elemento.innerHTML = el;
 
-    novo_elemento.addEventListener("click", (evt)=> {        // utilizando um escutador 'addEventListener' para o evento de click, chamar uma função anônima
-        caixa1.removeChild(evt.target);     // método 'removeChild' remove o filho de caixa1
+    novo_elemento.addEventListener("click", (evt)=> {
+        caixa1.removeChild(evt.target);
     });
     caixa1.appendChild(novo_elemento);
 });
