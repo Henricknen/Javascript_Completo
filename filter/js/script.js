f6d7a2626ca54e3546f492bdcb9e86fdefc36ace
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-12 20:39:15 -0300

    manipulando_o_DOM_refatorando_o_codigo_criando_funcao_'remove'_para_remover_hard_skill_selecionada

--- filter/js/script.js
+++ filter/js/script.js
@@ -1,11 +1,11 @@
 const caixa1 = document.querySelector("#caixa2");
 const array_skill = ["Javascript", "PHP", "Laravel", "ReactJs", "VueJs", "HTML", "CSS"];
-const btn_skill_selecionada = document.getElementById("btn_skill_selecionada");     // pegando botão do DOM
+const btn_remove_skill = document.getElementById("btn_remover_skill");     // pegando botão do DOM de id 'btn_remover_skill'
 
-array_skill.map((el, chave)=> {
-    const novo_elemento = document.createElement("div");    
+array_skill.map((el, chave) => {
+    const novo_elemento = document.createElement("div");
     novo_elemento.setAttribute("id", "skill" + chave);
-    novo_elemento.setAttribute("class", "hardskill");  
+    novo_elemento.setAttribute("class", "hardskill");
     novo_elemento.innerHTML = el;
 
     const comandos = document.createElement("div");
@@ -13,7 +13,6 @@ array_skill.map((el, chave)=> {
 
     const rd = document.createElement("input");
     rd.setAttribute("type", "radio");
-    rd.setAttribute("name", "rd_skill");
 
     comandos.appendChild(rd);
     novo_elemento.appendChild(comandos);
@@ -21,13 +20,31 @@ array_skill.map((el, chave)=> {
     caixa2.appendChild(novo_elemento);
 });
 
-btn_skill_selecionada.addEventListener("click", (evt)=> {      // inserindo evento de click no botão de id 'btn_skill_selecionada', e utilizando uma 'arrow function' para interçepitar o evnto 'evt'
-    const todos_radio = [...document.querySelectorAll("input[type=radio]")];        // pegando todos elementos que forem input do type 'radio' e transformando em array utilizando 'spread'
-    let radio_selecionado = todos_radio.filter((ele, ind, arr)=> {      // utilizando 'filter' para fazer a filtragem em todos radios
-        return ele.checked;     // retorna o elemento que estiver 'checked'
+const radio_selecionado = () => {       // função responsável de pegar o radio 'seleçionado'
+    const todos_radio = [...document.querySelectorAll("input[type=radio]")];
+    const radio_selecionado = todos_radio.filter((ele, ind, arr) => {
+        return ele.checked;
     });
 
-    radio_selecionado = radio_selecionado[0];       // variável 'radio_selecionado' somente da possição 0 ou seja somente o radio selecionado
-    const skill_selecionadaa = radio_selecionado.parentNode.previousSibling.textContent     // pegando o irmão anterior 'previousSibling' do parentNode apenas o texto 'textContent'
-    alert("HardSkill seleçionada: " + skill_selecionadaa);      // imprimindo em alert a 'skill_selecionadaa'
-}); 
+    return radio_selecionado[0];
+}
+
+btn_skill_selecionada.addEventListener("click", (evt) => {
+    const rs = radio_selecionado();      // 'rs' obtém o conteúdo da função 'radio_selecionado'
+    if(rs != undefined) {       // se 'rs' for diferente de 'undefined'
+        const skill_selecionadaa = rs.parentNode.previousSibling.textContent
+        alert("HardSkill seleçionada: " + skill_selecionadaa);      // apresenta um 'alert' com a skill seleçionada
+    } else {
+        alert("Seleçione uma Hard Skill...");
+    }
+});
+
+btn_remove_skill.addEventListener("click", (evt) => {
+    const rs = radio_selecionado();
+    if(rs != undefined) {
+        const skill_selecionadaa = rs.parentNode.parentNode
+        skill_selecionadaa.remove();        // utilizando função 'remove' para remover skill seleçionada
+    } else {
+        alert("Seleçione uma Hard Skill...");
+    }
+});
