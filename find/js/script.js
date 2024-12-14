ad96bb44071a4323c8e340d42f17ea938dc216bc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-13 23:11:47 -0300

    utilizando_o_metodo_'find'_para_pesquisar_em_array_de_string

--- find/js/script.js
+++ find/js/script.js
@@ -3,12 +3,12 @@ const txt_pesquisar = document.querySelector("#txt_pesquisar");
 const btn_pesquisar = document.querySelector("#btnPesquisar");
 const resultado = document.querySelector("#resultado");
 
-const elementos_array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];      // array 'numérico'
+const elementos_array = ["html", "css", "javascript"];      // array 'string'
 p_array.innerHTML = "[" + elementos_array + "]";        // preenchendo 'p_array' com o array 'numérico' criado
 
 btn_pesquisar.addEventListener("click", (evt)=> {       // utilizando um escutador 'addEventListener' para escutar o evento de click que chamará a arrow function
     elementos_array.find((e, i)=> {        // método find retorna o valor do primeiro elemento 'e' do array
-        if(e == txt_pesquisar.value) {        // verificando se o elemento pesquisado é igual ao elemento
+        if(e.toUpperCase() == txt_pesquisar.value.toUpperCase()) {
             resultado.innerHTML = "Valor encontrado " + e + " na posição " + i;      // preechendo variável 'resultado' com o valor pesquisado
             return e
         }
