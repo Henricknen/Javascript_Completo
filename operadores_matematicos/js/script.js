37af80824c52963b27fb0d1e4e4866453d4c0e97
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-15 17:29:52 -0300

    utilizando_'operadores_matematicos'_'+'_para_somar_duas_variaveis_'-'_faz_a_subtracao_e_'%'_que_mostra_o_resto_da_divisao

---
+++ operadores_matematicos/js/script.js
@@ -0,0 +1,15 @@
+let num1 = 0, num2 = 0, res1 = 0, res2 = 0;      // variáveis inicializando em 0
+
+num1 = 2;        // atribuindo valores as variaveis 'num1' e 'num2'
+num2 = 15;
+
+res1 = num1 + num2;       // fazendo a soma com o operador aritmetico '+'
+res2 = (num1 + num2) * 2;        // fazendo a multiplicação depois da soma
+
+console.log(res1);        // fazendo a impressao da soma
+console.log(res2);           // por usar () a multiplicação sera feita depois da soma
+console.log(num2 - num1);       // realizando 'subtração' de num1 e num2 direto no console
+
+res3 = num2 % num1;     // operador '%' pega o resto da divisão
+
+console.log(res3);
