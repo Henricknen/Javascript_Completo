ef3fc5d170a94ba4625d1e8f12473f2f478d5641
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-18 17:35:12 -0300

    operador_ternario'_e_uma_forma_de_'reduzir'_ou_'simplificar'_uma_condicional

---
+++ operador_ternario/js/script.js
@@ -0,0 +1,5 @@
+let num = 10    // 0 = False / 1 = True
+                 // verdadeiro / falso
+res = (!(num % 2) ? "Par" : "Impar")      // '?' é o operador ternario, '!' nega 'num % 2' retornaria 'false'
+
+console.log(res)        // retorna 'true' no console
