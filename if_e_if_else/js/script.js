818a1cd0589cc663735a20aa854a341ace16dab3
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-17 23:22:47 -0300

    utilizando_comandos_de_decisao_'if'_e_'if_else'_para_faz_devios_no_programa_dependendo_da_expressao_logica

---
+++ if_e_if_else/js/script.js
@@ -0,0 +1,11 @@
+let num = 10
+
+if(num > 10) {        // utilizando o 'if' seguida da expressão logica
+    console.log("Numero é maior que 10")        // se a expressão for verdadeira
+} else if(num > 5) {        // 'else if' equivale 'ou seja'
+    console.log("Numero está ente 6 e 10")        
+} else {
+    console.log("Numero menor ou igual a 5");       // se nehuma das expressões anteriores forem verdadeira será impresso esse 'console'
+}
+
+console.log("Fim do programa");
