71c83771d65ce256264374ffcb19b5bd3196f676
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-18 17:32:19 -0300

    Remove arquivo e adiciona diretório operador_ternario

--- operador_ternario.js
+++
@@ -1,5 +0,0 @@
-let num = 10    // 0 = False / 1 = True
-                 // verdadeiro / falso
-res = (!(num % 2) ? "Par" : "Impar")      // '?' é o operador ternario, '!' nega 'num % 2' retornaria 'false'
-
-console.log(res)        // retorna 'true' no console
