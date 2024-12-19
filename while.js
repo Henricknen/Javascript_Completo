686bca73b83bf10fb4b713eb45f04e050c691858
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-19 18:55:05 -0300

    criando_fatorial_utilizando_loop_'while'_significa_enquanto_e_um_loop_indefinido_pois_e_utilizado_quando_nao_se_tem_certeza_da_quantidade_de_x_que_sera_executado

---
+++ while.js
@@ -0,0 +1,9 @@
+let n = 10;     // variável 'n' sendo iniçializada com 10
+let fat = 1;
+
+while(n >= 1) {     // enquanto 'while' n for maior ou igual a 1
+    fat *= n;       // é exucutado ess instrução 'fat x n'
+    n--;        // 'decrementando' n
+}
+
+console.log(fat);       // dando console em 'fat' variável que reçeberá o fatorial
