37bfbb00845a0d885ce8cec77b43f9e474d16835
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-19 17:56:22 -0300

    criando_um_loop_definido_chamado_'for'_que_e_uma_estrutura_de_repeticao_que_repete_um_comando_ou_um_bloco_de_comandos_tal_repeticoes_sao_chamadas_de_'iteracoes'

---
+++ for.js
@@ -0,0 +1,5 @@
+let num = [10, 20, 30, 40, 50]      // array numerico
+
+for(let i = 0; i < num.length; i++) {       // o for necessita de três configurações, a iniçialização 'i = 0', condição de execução 'i < num.length' e o controle (incremento ou decremeto) 'i++' 
+    console.log(num[i])     // imprimime os elementos do array 'num' da posições indicadas por 'i' ex: 0 = 10, 1 = 20, 2 = 30, 3 = 40, 4 = 50
+}
