645ac4edaec736008943e76801c8ada162632d60
Luis Henrique Siva Ferreira <l.henrick@live.com>
2024-12-20 18:38:40 -0300

    loop_'do_while'_inicia_com_'do'_significando_'fazer'_ou_seja_'executa'_o_bloco_de_comandos_do_loop_e_no_final_da_estrutura_e_executado_o_'while'_teste_logico

---
+++ do_while.js
@@ -0,0 +1,7 @@
+let n = 10;
+do {        // 'do' é o mesmo que fazer
+    console.log("Execução dos comandos...");
+    n++
+} while(n < 10) {       // condição de parada
+    console.log("Fim do programa....")
+}
