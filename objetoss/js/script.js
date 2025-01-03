7420b70000644d3e872c352e581ec84b6021ca68
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-03 20:02:20 -0300

    criando_metodo_'tradicional'_chamado_info_que_impreme_no_console_informacoes_das_propriedades_do_objeto_e_metodos_getNome_getTipo_getVelMax_getInfo_que_obtem_e_retorna_as_propriedades_do_objeto

--- objetoss/js/script.js
+++ objetoss/js/script.js
@@ -16,6 +16,34 @@ class Carro {      // classe Carro
         this.velmax = 180;
        }
     }
+
+    getNome() {     // método 'getNome' retorna o nome que está definindo na 'propriedade' nome
+        return this.nome;
+    }
+
+    getTipo() {
+        return this.tipo;
+    }
+
+    getVelMax() {
+        return this.velmax;
+    }
+
+
+    getInfo() {     
+        return [this.nome, this.tipo, this.velmax];     // método 'getInfo' retorna as propriedades em um array 
+    }
+
+
+
+
+    info() {        // método 'info' retorna todas propriedade de uma única vez
+        console.log("----------------------------");
+        console.log(`Nome: ${this.nome}`);
+        console.log(`Tipo: ${this.tipo}`);
+        console.log(`V. Max: ${this.velmax}`);
+        console.log("----------------------------");
+    }
 }
 
 
@@ -26,9 +54,9 @@ let c4 = new Carro("Carro Forte", 4);
 
 console.log(c1.nome);       // imprimindo no console o nome do carro c1 chamando a 'propriedade' nome
 console.log(c1.tipo);           // imprimindo no console o tipo do carro c1 chamando a 'propriedade' tipo
-console.log(c2.nome);
-console.log(c2.tipo);
-console.log(c3.nome);
-console.log(c3.tipo);
-console.log(c4.nome);
-console.log(c4.tipo);
+
+c2.info();      // chamando o método 'info' que exibirá as informações do carro 2
+
+console.log(c3.getNome());      // imprimindo o nome do carro c3 utilizando método 'getNome'
+
+console.log(c4.getInfo());       // imprimindo o array com 'todas propriedades' de c4
