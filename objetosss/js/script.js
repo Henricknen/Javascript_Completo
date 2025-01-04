6a3ef217a75dcb6fb70d1843da456cc3171bf4dc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-03 21:25:43 -0300

    criando_metodos_para_'setar'_atribuir_valores_nas_propriedades_da_classe

---
+++ objetosss/js/script.js
@@ -0,0 +1,75 @@
+class Carro {      // classe Carro
+    constructor(pnome, ptipo) {     // passando no método 'construtor' o nome e o 'tipo' do 'carro' como parâmetro 
+       this.nome = pnome;
+
+       if(ptipo == 1) {     // executando 'condição' com base na 'propriedade' tipo do carro
+        this.tipo = "Esportivo";        // definindo 'propriedade' 'tipo' do carro
+        this.velmax = 300;      // definindo a velocidade máxima do carro utilizando 'propriedade' 'velmax'
+       } else if(ptipo == 2) {
+        this.tipo = "Ultilitário";
+        this.velmax = 100;
+       } else if(ptipo == 3) {
+        this.tipo = "Passeio";
+        this.velmax = 160;
+       } else {
+        this.tipo = "Militar";
+        this.velmax = 180;
+       }
+    }
+
+    getNome() {     // método 'getNome' retorna o nome que está definindo na 'propriedade' nome
+        return this.nome;
+    }
+
+    getTipo() {
+        return this.tipo;
+    }
+
+    getVelMax() {
+        return this.velmax;
+    }
+
+
+    getInfo() {     
+        return [this.nome, this.tipo, this.velmax];     // método 'getInfo' retorna as propriedades em um array 
+    }
+
+    setNome(nome) {     // método 'setNome' atribui um novo nome para propriedade nome
+        this.nome = nome;
+    }
+
+    setTipo(tipo) {
+        this.tipo = tipo;
+    }
+    
+    setVelMax(velmax) {
+        this.velmax = velmax;
+    }
+
+
+    info() {        // método 'info' retorna todas propriedade de uma única vez
+        console.log("----------------------------");
+        console.log(`Nome: ${this.nome}`);
+        console.log(`Tipo: ${this.tipo}`);
+        console.log(`V. Max: ${this.velmax}`);
+        console.log("----------------------------");
+    }
+}
+
+
+let c1 = new Carro("Ferrari", 1);       // instaciando um novo objeto passando o 'nome' e o 'tipo'
+let c2 = new Carro("Chevrolet", 2);
+let c3 = new Carro("Corolla", 3);
+let c4 = new Carro("Carro Forte", 4);
+
+c4.setNome("Blidado");      // chamando o método 'setNome' que irá setar ou seja alterar o nome da propriedade c4, que passará a ser chamado de 'Blindado'
+c4.setVelMax(300);
+
+console.log(c1.nome);       // imprimindo no console o nome do carro c1 chamando a 'propriedade' nome
+console.log(c1.tipo);           // imprimindo no console o tipo do carro c1 chamando a 'propriedade' tipo
+
+c2.info();      // chamando o método 'info' que exibirá as informações do carro 2
+
+console.log(c3.getNome());      // imprimindo o nome do carro c3 utilizando método 'getNome'
+
+console.log(c4.getInfo());       // imprimindo o array com 'todas propriedades' de c4
