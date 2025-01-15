d2947c3a2fe58a9795a9435a2a82355531ce6e40
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-14 21:43:18 -0300

    em_javascrit_criando_classe_Bola_para_definir_'tamanho'_'cores'_e_'posicao'_das_bolas_e_implementando_'eventos'_para_ajustar_o_calculo_de_posicao_e_cores

--- adicao_ou_remocao_de_objetos/js/script.js
+++ adicao_ou_remocao_de_objetos/js/script.js
@@ -1,4 +1,4 @@
-const palco = document.getElementById("palco");
+const palco = document.getElementById("palco");     // pegando elementos do DOM pelo 'id'
 const num_objetos = document.getElementById("num_objetos");
 const txt_qtd = document.getElementById("txt_qtd");
 const btn_add = document.getElementById("btn_add");
@@ -8,3 +8,32 @@ let larguraPalco = palco.offsetWidth;       // propriedade 'offsetWidth' pega a 
 let altura_Palco = palco.offsetHeight;          // propriedade 'offsetHeight' pega a altura
 let bolas = [];     // array
 let numBola = 0;
+
+class Bola {
+    constructor() {
+        this.tam = math.floor(Math.random() * 15) + 10;      // definindo o 'tamanho' da variável tam utilizando 'método' Math
+        this.r = math.floor(Math.random() * 255);       // definindo as cores
+        this.g = math.floor(Math.random() * 255);
+        this.b = math.floor(Math.random() * 255);
+        this.px = Math.floor(Math.random() * (larguraPalco - this.tam));        // define a posição de onde a bolinha ficará
+        this.py = Math.floor(Math.random() * (altura_Palco - this.tam));
+    }
+}
+
+window.addEventListener("resize", (evt)=> {     // adiçionando evento quando a tela for redimensionada
+    let larguraPalco = palco.offsetWidth;
+    let altura_Palco = palco.offsetHeight;     
+});
+
+btn_add.addEventListener("resize", (evt)=> {
+    const qtde = txt_qtde.value;        // obtendo a quantidade de objetos
+    for(let i = 0; i < qtde; i++) {
+        // instanciar novos objetos
+    }
+});
+
+btn_remover.addEventListener("resize", (evt)=> {
+    bolas.map((b)=> {       // utilizando função map para percorrer elemento por elemento que estiver dentro do array bolas
+        // criar função para remove bolinhas
+    });
+});
