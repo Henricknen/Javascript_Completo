f04a39dc78ba11c4971156b792231796226e04ee
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-14 22:41:09 -0300

    definindo_'velocidade_maxima'_'direcoes'_'id'_de_cada_bolinha_e_criando_metodos_para_pegar_a_'posicao'_'remover'_'desenhar'_e_'controlar'_a_bolinha

--- adicao_ou_remocao_de_objetos/js/script.js
+++ adicao_ou_remocao_de_objetos/js/script.js
@@ -10,13 +10,40 @@ let bolas = [];     // array
 let numBola = 0;
 
 class Bola {
-    constructor() {
+    constructor(arrayBolas, palco) {        // ao instançiar um objeto é necessário passar o 'arrayBolsas' e o 'palco'
         this.tam = math.floor(Math.random() * 15) + 10;      // definindo o 'tamanho' da variável tam utilizando 'método' Math
         this.r = math.floor(Math.random() * 255);       // definindo as cores
         this.g = math.floor(Math.random() * 255);
         this.b = math.floor(Math.random() * 255);
         this.px = Math.floor(Math.random() * (larguraPalco - this.tam));        // define a posição de onde a bolinha ficará
         this.py = Math.floor(Math.random() * (altura_Palco - this.tam));
+        this.velx = Math.floor(Math.random() * 2) + 0.5;        // definindo as 'veloçidades máximas'
+        this.vely = Math.floor(Math.random() * 2) + 0.5;
+        this.dirx = Math.random() * 10 > 5 ? 1 : -1;        // definindo as 'direções'
+        this.diry = Math.random() * 10 > 5 ? 1 : -1;
+        this.palco = palco;
+        this.arrayBolas = arrayBolas;
+        this.id = Date.now() + "_" + Math.floor(Math.random() * 100000000);
+        this.desenhar();        // chamando método 'desenhar'
+        this.controle = stInterval(this.controlar, 10);
+        this.controle = document.getElementById(this.id);
+        this.eu = ""
+    }
+
+    minhaPos =()=> {        // método 'minhaPos' verifica qual é a posição da bolinha
+
+    }
+
+    remover =()=> {     // método 'remover' remove a bolinha
+
+    }
+
+    desenhar =()=> {        // método 'desenhar' desenha, cria a bolinha
+        
+    }
+
+    controlar =()=> {       // método 'controlar' controla a movimentação da bolinha
+
     }
 }
 
