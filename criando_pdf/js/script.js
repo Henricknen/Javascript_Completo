91ae9708dd1b8a3edebc33b16e2974299b6a5bb5
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-17 21:11:34 -0300

    finalizando_sistema_que_'cria_pdf'_imprementando_logica_para_imprimi_lo

--- criando_pdf/js/script.js
+++ criando_pdf/js/script.js
@@ -1,5 +1,22 @@
 const btn_imp = document.getElementById("btn_imp");
 
 btn_imp.addEventListener("click", (evt)=> {
-    
+    const conteudo = document.getElementById("tabela");
+
+    let estilo = "<style>";     // criando um estilo para a página da aba de impressão
+    estilo += "table {width: 100%;font: 25px Calibri;}";
+    estilo += "table, th, td {border: solid 2px #888; border-collapse: collapse;}";
+    estilo += "padding: 4px 8px;text-align: center;";
+    estilo += "</style>";
+
+    const win = window.open('', '', 'height=700,width=700');        // 'window.open' cria 'nova janela'
+
+    win.document.write('<html><head>');     // utilizando 'write' para escrever dentro da nova janela
+    win.document.write('<title>Javascript</title>');
+    win.document.write(estilo);
+    win.document.write('</head><body>');
+    win.document.write(conteudo.innerHTML);
+    win.document.write('</body></html>');
+
+    win.print();
 });
