b96d3edc1997583a3b64ddb9a2b261e9dc859d04
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-21 20:48:13 -0300

    criando_funcao_que_retorna_todas_minhas_formacoes_e_definindo_export_da_mesma_com_padrao_utilizando_'default'_deixando_o_codigo_mais_limpo

---
+++ modulos_/js/script.js
@@ -0,0 +1,5 @@
+import { formacoes } from "../md/formacao.js";
+import getTodasFormacoes from "../md/formacao.js";       // 'getTodasFormacoes' por ser padrão não é necessario colocar entre chaves '{}' deixando o código mais limpo
+
+console.log(formacoes);
+console.log(getTodasFormacoes());
---
+++ modulos_/md/formacao.js
@@ -0,0 +1,8 @@
+const formacoes = ["Pós Desenvolvimento Web", "Superior Análise e Desenvolvimento de Sistemas", "Técnico em Informática para Internet", "Técnico Eletroeletrônica", "Curso Front End", "Curso Lógica de Programação"];
+
+const getTodasFormacoes =()=> {     // funcão retorna tadas minhas formações
+    return formacoes;
+}
+
+export {formacoes}
+export default getTodasFormacoes;       // funcão 'getTodasFormacoes' é o export padrão deste modulo
