850474a09a26ad43d3d091c978db43825dd42907
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-20 21:42:14 -0300

    'modulos'_e_uma_maneira_de_'reaproveitar'_'organizar'_e_'distribuir'_codigos

---
+++ modulos/js/script.js
@@ -0,0 +1,6 @@
+import { formacoes } from "../md/formacao.js";     // importando 'formacoes'
+import { tecnologias } from "../md/formacao.js";
+
+console.log("Executando Servidor");
+console.log(formacoes);     // imprimindo no console elemento 'formacoes' que está sendo imprementado em outro arquivo
+console.log(tecnologias);
---
+++ modulos/md/formacao.js
@@ -0,0 +1,4 @@
+const formacoes = ["Pós Desenvolvimento Web", "Superior Análise e Desenvolvimento de Sistemas", "Técnico em Informática para Internet", "Técnico Eletroeletrônica", "Curso Front End", "Curso Lógica de Programação"];
+const tecnologias = ["PHP", "Javascript", "Laravel", "ReactJs", "MySql"];
+
+export {formacoes, tecnologias}      // exportando o componente 'formacoes' e 'tecnologias'
