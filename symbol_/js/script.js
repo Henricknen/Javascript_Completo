4c7f888e3ae14afa77f9b0009dfbbae461e6e204
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-25 21:37:11 -0300

    removendo_elemento_pelo_nome_e_utilizando_metodo_'map'_para_retornar_o_symbol_de_jogador_especifico

--- symbol_/js/script.js
+++ symbol_/js/script.js
@@ -5,13 +5,25 @@
     }
 }
 
-let jogadores = [new Jogador("j1"), new Jogador("j2"), new Jogador("j3"), new Jogador("j4")]
+let jogadores = [new Jogador("j1"), new Jogador("j2"), new Jogador("j3"), new Jogador("j4"), new Jogador("j3"), new Jogador("j1"), new Jogador("j3")];
 
 let  s1 = jogadores[0].id;      // 'reçebendo' o symbol do jogador da posição 0
+let s = [];
+
+let s_jogadores =jogadores.filter((j)=> {
+    return j.nome == "j1";      // retorna apenas os jogadores com nome igual a j1
+});
 
 jogadores = jogadores.filter((j)=> {    // utilizando 'filter' para remover jogador
-    return j.id != s1;      // retornando apenas jogadores com id 'diferente' de s1
+    // return j.id != s1;      // retornando apenas jogadores com id 'diferente' de s1
+    return j.nome != "j1";
+});
+
+s = s_jogadores.map((j)=> {
+    return j.id     // retorna os symbol's do jogador j1
 });
 
 console.log(jogadores);
+console.log(s_jogadores);
 console.log(s1);
+console.log(s);
