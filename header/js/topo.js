2d5aab0d078d933ba54387c3a37b35fcb90f82bc
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-02-11 20:47:05 -0300

    fazendo_comentarios_explicativos_para_melhor_compressao_do_codigo

--- header/js/topo.js
+++ header/js/topo.js
@@ -1,22 +1,22 @@
-const head = document.head;
+const head = document.head;     // assoçiando elemento head na variável 'head'
 const body = document.body;
 
-const estilo = "</link rel = 'styleshet' 'text/css' href = 'topo.css'/>";
-head.innerHTML += estilo;
+const estilo = "</link rel = 'styleshet' 'text/css' href = 'topo.css'/>";       // link do arquivo topo.css na variável estilo
+head.innerHTML += estilo;       // assoçiando arquivo css no head
 
-const topo = document.createElement("div");
-topo.setAttribute("id", "topo");
+const topo = document.createElement("div");     // criando uma 'div' topo
+topo.setAttribute("id", "topo");        // inserindo atributo 'id'
 topo.setAttribute("class", "topo");
-body.prepend(topo);
+body.prepend(topo);     // 'anexando' div criada em body
 
-const logo = 
+const logo =        // criando 'logo'
 "<div id = 'logo' class = 'login'>" +
     "<img src = 'logo.png' title = 'Header'/>" +
 "</div>";
 
-topo.innerHTML += logo;
+topo.innerHTML += logo;     // inserindo logo 'dentro' do topo
 
-const login = 
+const login =       // area de login
 "<div id = 'login' class = 'login'>" +
 "<p id = 'matricula'>Matrícula:<span></p>" +
 "<p id = 'nome'>Nome:<span></span></p >" +
