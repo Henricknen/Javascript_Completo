75ef638e6135c81a72ea95e3ae4e281c1dfef96d
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-03-05 21:58:38 -0300

    criando_as_imagens_das_funcionalidades_de_'excluir'_'editar'_e_'visualizar'

--- DataGridView/js/script.js
+++ DataGridView/js/script.js
@@ -5,6 +5,7 @@ const configdgv = {     // arquivo de configuração
 
 const dgv =(configdgv)=> {
     const dgvDados = document.getElementById(configdgv.idDestino);     // destino do dados
+    // dgvDados = innerHTML = "";      // 'limpando' as linhas do dataGridView antes de carregar os dados
     fetch(configdgv.endpoint)     // 'consumindo' o endpoint
     .then(res=> res.json())
     .then(res=> {
@@ -13,29 +14,43 @@ const dgv =(configdgv)=> {
             dgvLinha.setAttribute("class", "dgvLinha");
 
             const c1 = document.createElement("div");       // criando uma 'div'
-            c1.setAttribute("class", "c1");
+            c1.setAttribute("class", "coluna c1");
             c1.innerHTML = el.id;
             dgvLinha.appendChild(c1);
 
             const c2 = document.createElement("div");
-            c2.setAttribute("class", "c2");
+            c2.setAttribute("class", "coluna c2");
             c2.innerHTML = el.produto;
             dgvLinha.appendChild(c2);
 
             const c3 = document.createElement("div");
-            c3.setAttribute("class", "c3");
+            c3.setAttribute("class", "coluna c3");
             c3.innerHTML = el.marca;
             dgvLinha.appendChild(c3);
 
             const c4 = document.createElement("div");
-            c4.setAttribute("class", "c4");
+            c4.setAttribute("class", "coluna c4");
             c4.innerHTML = el.modelo;
             dgvLinha.appendChild(c4);
 
             const c5 = document.createElement("div");
-            c5.setAttribute("class", "c5");
-            c5.innerHTML = "D E V";
+            c5.setAttribute("class", "coluna c5");
             dgvLinha.appendChild(c5);
+
+            const imgDelete = document.createElement("img");
+            imgDelete.setAttribute("class", "dgvIcone");
+            imgDelete.setAttribute("src", "deletar.svg");
+            c5.appendChild(imgDelete);
+
+            const imgEditar = document.createElement("img");
+            imgEditar.setAttribute("class", "dgvIcone");
+            imgEditar.setAttribute("src", "editar.svg");
+            c5.appendChild(imgEditar);
+
+            const imgExibir = document.createElement("img");
+            imgExibir.setAttribute("class", "dgvIcone");
+            imgExibir.setAttribute("src", "exibir.svg");
+            c5.appendChild(imgExibir);
 
             dgvDados.appendChild(dgvLinha);
         });
