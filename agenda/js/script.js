2127ef273195a1f3a2f2f0b4dd217fe7228005ed
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-03-07 20:58:45 -0300

    'gerenciando'_as_abas_do_menu_passando_a_'selecao'_para_a_aba_selecionada

--- agenda/js/script.js
+++ agenda/js/script.js
@@ -8,21 +8,30 @@ const btn_sobre = document.querySelector("#btn_sobre");
 const principal = document.querySelector("#principal");
 
 btn_home.addEventListener("click", (evt)=> {
-    window.open("./home.html", "if_principal");      // utilizando 'window.open' para fazer o redireçionamento pro arquivo do primeiro parâmetro e para o local do arquivo espeçificado no segundo parâmetro
+    abrir_pagina(evt.target, "./home.html");
 });
 
 btn_novo.addEventListener("click", (evt)=> {
-    window.open("./novo.html", "if_principal");
+    abrir_pagina(evt.target, "./novo.html");
 });
 
 btn_pesquisar.addEventListener("click", (evt)=> {
-    window.open("./pesquisar.html", "if_principal");    
+    abrir_pagina(evt.target, "./pesquisar.html");    
 });
 
 btn_gestao.addEventListener("click", (evt)=> {
-    window.open("./gestao.html", "if_principal");    
+    abrir_pagina(evt.target, "./gestao.html");    
 });
 
 btn_sobre.addEventListener("click", (evt)=> {
-    window.open("./sobre.html", "if_principal");    
+    abrir_pagina(evt.target, "./sobre.html");
 });
+
+const abrir_pagina =(el, url)=> {      // fução 'abrir_pagina' reçebe como parâmetro um elemento que será seleçionado
+    const abas = [...document.querySelectorAll(".aba")];     // pegando todos elementos que contém a 'classe aba'
+    abas.forEach(e=> {      // percorrendo elemento por elemento do array 'abas' e adiçionando em 'e'
+        e.classList.remove("aba_selecionada");      // removendo todas abas seleçionadas
+    });
+    el.classList.add("aba_selecionada");        // adiçionando em 'el' a aba seleçionada       // adiçionando aba seleçionada
+    window.open(url, "if_principal");       // gerenciando a abertura pela aba seleçionada
+}
